9b3257705d425d647f489a5ccfe1ef6dc42ff7ec
Kharristov <annawey486@gmail.com>
2024-12-04 16:23:16 +0700

    Добавьте файлы проекта.

---
+++ 5c5.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+string convertToBase(int decimal, int q) {
+    string result = "";
+    string digits = "0123456789abcdefghijklmnopqrstuvwxyz";  
+
+    while (decimal > 0) {
+        result += digits[decimal % q];  
+        decimal /= q;
+    }
+    reverse(result.begin(), result.end());  
+    return result;
+}
+
+int main() {
+    int p, q;
+    string N;
+
+    cin >> p >> q >> N; 
+
+   
+    int decimal_value = stoi(N, nullptr, p);
+
+    cout << convertToBase(decimal_value, q) << endl;
+
+    return 0;
+}
